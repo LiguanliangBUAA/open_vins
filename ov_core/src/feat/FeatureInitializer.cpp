96560790f57987ec57beb8b6d32d44e3603135da
liguanliang <guanliang.li@alumnos.upm.es>
2026-07-08 11:29:33 +0200

    anchor tie situation

--- ov_core/src/feat/FeatureInitializer.cpp
+++ ov_core/src/feat/FeatureInitializer.cpp
@@ -37,7 +37,8 @@ bool FeatureInitializer::single_triangulation(std::shared_ptr<Feature> feat,
   size_t most_meas = 0;
   for (auto const &pair : feat->timestamps) {
     total_meas += (int)pair.second.size();
-    if (pair.second.size() > most_meas) {
+    if (pair.second.size() > most_meas ||
+        (pair.second.size() == most_meas && pair.first == 0)) {
       anchor_most_meas = pair.first;
       most_meas = pair.second.size();
     }
@@ -62,7 +63,7 @@ bool FeatureInitializer::single_triangulation(std::shared_ptr<Feature> feat,
       feat->p_FinA << uvn(0) * zs.at(m), uvn(1) * zs.at(m), zs.at(m);
       feat->p_FinG = R_GtoA.transpose() * feat->p_FinA + p_AinG;
       stat_depth_init_cnt++;
-      if ((stat_depth_init_cnt + stat_tri_init_cnt) % 50 == 0)
+      // if ((stat_depth_init_cnt + stat_tri_init_cnt) % 50 == 0)
         PRINT_INFO(CYAN "[INIT STATS] depth %d | tri %d (%.1f%% depth)\n" RESET,
                    stat_depth_init_cnt.load(), stat_tri_init_cnt.load(),
                    100.0 * stat_depth_init_cnt / (stat_depth_init_cnt + stat_tri_init_cnt));
